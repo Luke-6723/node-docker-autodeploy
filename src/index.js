9ec82d7e068a7e64e3475606c92cef70bc6ecc05
Luke-6723 <luke@ichigo.uk>
2020-12-18 00:14:16 +0000

    Base endpoint handling

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// initialize .env
+require('dotenv').config()
+
+// Dependencies
+const express = require('express')
+const bodyparser = require('bodyparser')
+const app = express()
+
+// Body parser middleware
+app.use(bodyparser.json())
+
+// Github ping route
+app.get('/github/autodeploy', (req, res) => {
+  res.status(200).send('OK')
+})
+
+// Post endpoint for github webhook
+app.post('/github/autodeploy', (req, res) => {
+  console.log(req.body)
+})
+
+// Allow lowercase and uppercase
+const PORT = Number(process.env.PORT) || Number(process.env.port)
+const HOST = process.env.HOST || process.env.host
+
+// Listen
+app.listen(PORT, HOST, () => {
+  console.log(`Listening on ${HOST}:${PORT}`)
+})
